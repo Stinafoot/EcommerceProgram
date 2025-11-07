a62dfc1f9e3c9a4e6db00b615fc687210faddcaf
LilacLuLu31 <b.belindal31@gmail.com>
2025-11-07 14:57:01 -0600

    Add files via upload

--- inventory.py
+++ inventory.py
@@ -1,4 +1,27 @@
 class Inventory:
 
-    def __init__(self):
+    def __init__(self, databaseName="methods.db"):
+        self.databaseName = databaseName
         sample = ""
+
+    def Inventory():
+    
+    def Inventory(self, databaseName):
+        
+    def viewInventory(self):
+
+    def searchInventory(self):
+
+    def decreaseStock(self, ISBN, quantity=1)
+        try:
+            connection = sqlite3.connect(self.databaseName)
+        except:
+            print("Failed database connection.")
+            sys.exit()
+
+        query = "SELECT stock FROM inventory WHERE ISBN = ?", (ISBN)
+        result = query 
+        if stock < quantity:
+            print("Not enough copies in stock")
+            return
+        new_stock = stock - quantity
